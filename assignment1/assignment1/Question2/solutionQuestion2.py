7b2dcfe1cbe27f5b8f3240456433b46288c6ee20
NattyEddy <heyitzn8@gmail.com>
2024-10-09 17:39:58 -0600

    Changes to Soln 2 meant to improve readability (added comments, refactored some variables, spaced things out).

--- assignment1/assignment1/Question2/solutionQuestion2.py
+++ assignment1/assignment1/Question2/solutionQuestion2.py
@@ -7,54 +7,62 @@
 #Input: The given input string
 #Output: The "clean" version of the input string without characters specified in the problem statement like ?, !, etc
 def clean_input_string(string):
-    newString = string
     peskyPunctuation = [".", ",", "!", "?", "\'"]
+    
     for punctuation in peskyPunctuation:
-        newString = newString.replace(punctuation, "")
-    return newString
+        string = string.replace(punctuation, "")
+    
+    return string
 
 #Input: The output from clean_input_string()
 #Output: The reversed version of the input string
 def reverse_string(string):
     newString = ""
     splicedString = string.split()
-    for i in range(len(splicedString)):
-        if newString != "": newString += " "
-        newString += splicedString[~i]
+    
+    for i in range(len(splicedString)): # from first to last elmt in list splicedString
+        if newString != "": newString += " " # add space where necessary
+        newString += splicedString[~i] # from last to first elmt in list, add word into string newString
+    
     return newString
 
 #Input: The output from reverse_string()
 #Output: A string with all the duplicate occurrences of words removed. Only the first occurrence will remain in the string
 def remove_duplicates(string):
     newString = ""
-    stringList = string.split()
-    listOfWords = []
-    for word in stringList:
-        if word not in listOfWords: listOfWords.append(word)
-    for word in listOfWords:
-        if newString != "": newString += " "
+    splicedString = string.split()
+    nonDuplicates = []
+    
+    for word in splicedString:
+        if word not in nonDuplicates: nonDuplicates.append(word)
+    
+    for word in nonDuplicates:
+        if newString != "": newString += " " # add space where necessary
         newString += word
+    
     return newString
     
 #Input: The output from remove_duplicates()
 #Output: The median length of the words in the input string. This function must return an integer, more specifically the floor value.
 def calculate_median_length(string):
-    median = 0
-    medianList = []
-    stringList = string.split()
-    for word in stringList: medianList.append(len(word))
-    medianList.sort()
-    size = len(medianList)
-    if size % 2 == 0: median = (medianList[(size + 1) // 2] + medianList[(size // 2)]) // 2
-    else: median = medianList[(size + 1) // 2]
-    # median = median // len(stringList)
+    characterCounts = []
+    sentence = string.split()
+    for word in sentence: characterCounts.append(len(word)) # assign the character count of each word
+    characterCounts.sort()
+    size = len(characterCounts)
+    
+    # the following is the algorithm that uses the median formula
+    if size % 2 == 0: 
+        median = (characterCounts[(size + 1) // 2] 
+                + characterCounts[(size // 2)]) // 2
+    else: median = characterCounts[(size + 1) // 2]
+    
     return median
 
 def main():
     inputString = input()
 
     cleanInputString = clean_input_string(inputString)
-    # print(cleanInputString)
     
     reversedString = reverse_string(cleanInputString)
     print(reversedString)
@@ -68,4 +76,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-
