ce530d6953a2935f31518f27312a6fd95c0d4026
NattyEddy <heyitzn8@gmail.com>
2024-10-13 17:25:07 -0600

    Assignment 2 added to repository.

---
+++ assignment2/Question1/solutionQuestion1.py
@@ -0,0 +1,15 @@
+# Name: <replace with your name>
+# ccid: <replace with your ccid>
+# studentId: <replace with your student id>
+# operating system: <replace with yours>
+# python version: <replace with your python version>
+
+def fractal_eval(expr):
+    pass
+                
+def main():
+    exp = input()
+    print(round(fractal_eval(exp)))
+
+if __name__ == "__main__":
+    main()
---
+++ assignment2/Question2/solutionQuestion2.py
@@ -0,0 +1,19 @@
+# Name: <replace with your name>
+# ccid: <replace with your ccid>
+# studentId: <replace with your student id>
+# operating system: <replace with yours>
+# python version: <replace with your python version>
+
+def simplify_path(path):
+    # Add your implementation here
+    pass
+    
+def main():
+    # Takes Unix path as input
+    path = input()
+
+    # Simplify the path
+    print(simplify_path(path))
+
+if __name__ == "__main__":
+    main()
