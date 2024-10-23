4b6e8bcdc6fc01dfdbe674da9bcbb691ffe70018
NattyEddy <heyitzn8@gmail.com>
2024-10-22 19:23:22 -0600

    Soln 1 functionally complete.

--- assignment2/Question1/solutionQuestion1.py
+++ assignment2/Question1/solutionQuestion1.py
@@ -1,11 +1,18 @@
-# Name: <replace with your name>
-# ccid: <replace with your ccid>
-# studentId: <replace with your student id>
-# operating system: <replace with yours>
-# python version: <replace with your python version>
+# Name: Nathan Edillon
+# ccid: nedillon
+# studentId: 1826864
+# operating system: Fedora Linux
+# python version: 3.12.6
 
 def fractal_eval(expr):
-    pass
+    if "[" in expr and "]" in expr:
+        firstSplit = expr.split("[", 1)
+        left, enclosedInSquare = firstSplit[0], firstSplit[-1] # enclosedInSquare is right of the [
+        secondSplit = enclosedInSquare.rsplit("]", 1)
+        enclosedInSquare, right = secondSplit[0], secondSplit[-1] # enclosedInSquare is left of the ]
+        squareOfEnclosed = fractal_eval(enclosedInSquare) ** 2
+        expr = left + str(squareOfEnclosed) + right
+    return eval(expr)
                 
 def main():
     exp = input()
