eb8c41766536acdb3ed176fbca626a5358b87ca1
NattyEddy <heyitzn8@gmail.com>
2024-10-24 17:34:59 -0600

    Changes to improve readability.

--- assignment2/Question2/solutionQuestion2.py
+++ assignment2/Question2/solutionQuestion2.py
@@ -9,21 +9,35 @@ def simplify_path(path):
     pathList = list(path)
     path = ""
     stack = []
-    if pathList[0] == "/":
+    if pathList[0] == "/": # when the provided path is valid
+        
+        # analyze each character in the pathList and pop accordingly
         for char in range(len(pathList)):
             stack.append(pathList[char])
+            
+            # case: for slashes that are at the end of the path
+            ## it's important this if statement executes first so that later
+            ## cases assuming there are more characters don't cause an exception
             if pathList[char] == "/" and char + 1 == len(pathList): 
                 if stack.count("/") > 1: stack.pop()
-            elif pathList[char] == "/" and pathList[char + 1] == "." and pathList[char + 2] == ".": 
-                if stack.count("/") > 1: stack.pop(); stack.reverse()
+            
+            # case: for slashes followed by two periods
+            elif pathList[char] == "/" and pathList[char + 1] == "." and pathList[char + 2] == "." and stack.count("/") > 1: 
+                stack.pop(); stack.reverse()
                 lastSlashIndex = stack.index("/"); stack.reverse()
                 lastSlashIndex = len(stack) + ~lastSlashIndex
                 while len(stack) > lastSlashIndex:
                     stack.pop()
-            elif pathList[char] == "/" and (pathList[char + 1] == "/" or pathList[char + 1] == ".") or pathList[char] == ".": stack.pop()
+            
+            # cases: for slashes followed by another slash or a period, or a period
+            elif pathList[char] == "/" and (pathList[char + 1] == "/" or pathList[char + 1] == ".") or pathList[char] == ".": 
+                stack.pop()
+        
+        # append the approved stack to the string
         for char in stack:
             path += char
         return path
+    
     else: return "Invalid Path"
     
 def main():
