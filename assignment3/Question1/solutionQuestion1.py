1698772bee114d604bd8322190045528aa8c6b39
NattyEddy <heyitzn8@gmail.com>
2024-11-13 22:45:49 -0700

    Committing for later reference, gonna go back to the drawing board...

--- assignment3/Question1/solutionQuestion1.py
+++ assignment3/Question1/solutionQuestion1.py
@@ -2,18 +2,17 @@
 # ccid: nedillon
 # studentId: 1826864 
 # operating system: Fedora Linux 41 
-# python version: 3.12.6
+# python version: 3.13.0
 from treenode import TreeNode
 
-def construct_tree(length_of_tree, max_children, iterable, iteration=0, root=None): 
-    while iteration < length_of_tree:
-        if root == None:
-            root = TreeNode(next(iterable)); iteration += 1; continue
-        elif len(root.get_children()) != max_children:
-            root.add_child(TreeNode(next(iterable))); iteration += 1; continue
-        else:
-            for child in root.get_children():
-                construct_tree(length_of_tree, max_children, iterable, iteration, child)
+def construct_tree(max_children, node, root): 
+    if root == None:
+        root = TreeNode(node)
+    elif len(root.get_children()) == int(max_children):
+        return root
+        # child = construct_tree(max_children, node, child)
+    else:
+        root.add_child(TreeNode(node))
     return root
 
 def read_tree():
@@ -35,8 +34,10 @@ def read_tree():
     tree_criteria = input().split()
     length_of_tree, max_nodes = tree_criteria[0], tree_criteria[1]
     tree_array = input().split()
-    # print(tree_array)
-    return construct_tree(length_of_tree, max_children, iter(tree_array))
+    root = None
+    for node in tree_array:
+        root = construct_tree(max_nodes, node, root)
+    return root
 
 def main():
     # Do not modify
